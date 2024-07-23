e7fe23ed4989ffc3c975f7dc385b8524c365bd1e
basit <ba064251@gmail.com>
2024-07-23 12:32:52 +0500

    third

--- lib/Screens/ShowData.dart
+++ lib/Screens/ShowData.dart
@@ -13,51 +13,58 @@ class _ShowDataState extends State<ShowData> {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('product').snapshots(),
-          builder: (context, snapshot) {
-            if(snapshot.connectionState==ConnectionState.waiting)
-              {
-                return CircularProgressIndicator();
-              }
-            if(snapshot.hasError)
-              {
-                return Center(
-                  child: Icon(Icons.error,color: Colors.red,size: 30,),
-                );
-              }
-            if(snapshot.hasData)
-              {
-                return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 65,
-                              backgroundImage: NetworkImage('${snapshot.data!.docs[index]["product_image"]}'),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              StreamBuilder(
+                stream: FirebaseFirestore.instance.collection('product').snapshots(),
+                builder: (context, snapshot) {
+                  if(snapshot.connectionState==ConnectionState.waiting)
+                    {
+                      return CircularProgressIndicator();
+                    }
+                  if(snapshot.hasError)
+                    {
+                      return Center(
+                        child: Icon(Icons.error,color: Colors.red,size: 30,),
+                      );
+                    }
+                  if(snapshot.hasData)
+                    {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 65,
+                                    backgroundImage: NetworkImage('${snapshot.data!.docs[index]["product_image"]}'),
+                                  ),
+                                  SizedBox(width: 20,),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text('${snapshot.data!.docs[index]['product_name']}'),
+                                      Text('${snapshot.data!.docs[index]['product_price']}'),
+                                      Text('${snapshot.data!.docs[index]['product_qty']}'),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                            SizedBox(width: 20,),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('${snapshot.data!.docs[index]['product_name']}'),
-                                Text('${snapshot.data!.docs[index]['product_price']}'),
-                                Text('${snapshot.data!.docs[index]['product_qty']}'),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    );
-                  },);
-              }
-            return Container();
-          },
+                          );
+                        },);
+                    }
+                  return Container();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
